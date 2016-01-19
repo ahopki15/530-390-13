1229f608241de4bf22364da1b70e5eca58d7c102
ahopki15 <hopkinsaustin@gmail.com>
2016-01-19 12:35:21 -0500

    HW

--- assignments/assignment6/assignment62.py
+++ assignments/assignment6/assignment62.py
@@ -9,15 +9,23 @@ import assignment61 as a61
 #for i in range(10):
 #  A[i] = i
 
-N = 100000
-beta = .4
-A = np.zeros(N)
-for i in range(N):
-  A[i] = a61.exponential(beta)
+#N = 100000
+#beta = .4
+#A = np.zeros(N)
+#for i in range(N):
+#  A[i] = a61.exponential(beta)
 
-[X,H] = a61.histogram(A,10)
-plt.scatter(X,H)
-plt.show()
+#[X,H] = a61.histogram(A,10)
+#plt.figure()
+#plt.scatter(X,H)
+#plt.show()
+#plt.figure()
+#plt.plot(H)
+#plt.show()
 
 [I,S] = a61.monte_carlo_3d(a61.density,[-.5,-.5,-1],[1,1,1],a61.sphere,100000)
-print(I,S)
+[x,Sx] = a61.monte_carlo_3d(a61.xmoment,[-.5,-.5,-1],[1,1,1],a61.sphere,100000)
+[y,Sy] = a61.monte_carlo_3d(a61.ymoment,[-.5,-.5,-1],[1,1,1],a61.sphere,100000)
+[z,Sz] = a61.monte_carlo_3d(a61.zmoment,[-.5,-.5,-1],[1,1,1],a61.sphere,100000)
+
+print(x/I,y/I,z/I)
